46c1a24c11f1e68c61c2c7f8fd0bad18c412ef26
Danya <ermakov1709@mail.ru>
2023-04-04 21:00:31 +0300

    Добавьте файлы проекта.

---
+++ Turing/MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Turing
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddState = new System.Windows.Forms.Button();
+            this.RemoveState = new System.Windows.Forms.Button();
+            this.AlphabetTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.MakeStepButton = new System.Windows.Forms.Button();
+            this.InputTable = new System.Windows.Forms.DataGridView();
+            this.StateTable = new System.Windows.Forms.DataGridView();
+            this.StopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.InputTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StateTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddState
+            // 
+            this.AddState.Location = new System.Drawing.Point(646, 231);
+            this.AddState.Margin = new System.Windows.Forms.Padding(4);
+            this.AddState.Name = "AddState";
+            this.AddState.Size = new System.Drawing.Size(190, 48);
+            this.AddState.TabIndex = 1;
+            this.AddState.Text = "Добавить состояние";
+            this.AddState.UseVisualStyleBackColor = true;
+            this.AddState.Click += new System.EventHandler(this.AddState_Click);
+            // 
+            // RemoveState
+            // 
+            this.RemoveState.Location = new System.Drawing.Point(646, 287);
+            this.RemoveState.Margin = new System.Windows.Forms.Padding(4);
+            this.RemoveState.Name = "RemoveState";
+            this.RemoveState.Size = new System.Drawing.Size(190, 48);
+            this.RemoveState.TabIndex = 2;
+            this.RemoveState.Text = "Убрать состояние";
+            this.RemoveState.UseVisualStyleBackColor = true;
+            this.RemoveState.Click += new System.EventHandler(this.RemoveState_Click);
+            // 
+            // AlphabetTextBox
+            // 
+            this.AlphabetTextBox.Location = new System.Drawing.Point(15, 189);
+            this.AlphabetTextBox.Name = "AlphabetTextBox";
+            this.AlphabetTextBox.Size = new System.Drawing.Size(617, 33);
+            this.AlphabetTextBox.TabIndex = 3;
+            this.AlphabetTextBox.TextChanged += new System.EventHandler(this.AlphabetTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Алфавит";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(646, 17);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(190, 48);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "Запуск";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_ClickAsync);
+            // 
+            // MakeStepButton
+            // 
+            this.MakeStepButton.Location = new System.Drawing.Point(646, 71);
+            this.MakeStepButton.Name = "MakeStepButton";
+            this.MakeStepButton.Size = new System.Drawing.Size(190, 48);
+            this.MakeStepButton.TabIndex = 7;
+            this.MakeStepButton.Text = "Сделать шаг";
+            this.MakeStepButton.UseVisualStyleBackColor = true;
+            this.MakeStepButton.Click += new System.EventHandler(this.MakeStepButton_Click);
+            // 
+            // InputTable
+            // 
+            this.InputTable.AllowUserToAddRows = false;
+            this.InputTable.AllowUserToDeleteRows = false;
+            this.InputTable.AllowUserToResizeColumns = false;
+            this.InputTable.AllowUserToResizeRows = false;
+            this.InputTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.InputTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.InputTable.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.InputTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.InputTable.Location = new System.Drawing.Point(15, 17);
+            this.InputTable.Name = "InputTable";
+            this.InputTable.RowHeadersVisible = false;
+            this.InputTable.RowHeadersWidth = 30;
+            this.InputTable.Size = new System.Drawing.Size(617, 84);
+            this.InputTable.TabIndex = 24;
+            this.InputTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.InputTable_CellClick);
+            this.InputTable.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.InputTable_CellValidating);
+            // 
+            // StateTable
+            // 
+            this.StateTable.AllowUserToAddRows = false;
+            this.StateTable.AllowUserToDeleteRows = false;
+            this.StateTable.AllowUserToResizeColumns = false;
+            this.StateTable.AllowUserToResizeRows = false;
+            this.StateTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.StateTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.StateTable.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.StateTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StateTable.Location = new System.Drawing.Point(15, 231);
+            this.StateTable.Name = "StateTable";
+            this.StateTable.RowHeadersVisible = false;
+            this.StateTable.Size = new System.Drawing.Size(617, 253);
+            this.StateTable.TabIndex = 25;
+            this.StateTable.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.StateTable_CellValidating);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(646, 125);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(190, 48);
+            this.StopButton.TabIndex = 26;
+            this.StopButton.Text = "Остановить";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 499);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StateTable);
+            this.Controls.Add(this.InputTable);
+            this.Controls.Add(this.MakeStepButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AlphabetTextBox);
+            this.Controls.Add(this.RemoveState);
+            this.Controls.Add(this.AddState);
+            this.Font = new System.Drawing.Font("Noto Sans Cond", 14.25F, System.Drawing.FontStyle.Bold);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Машина Тьюринга";
+            ((System.ComponentModel.ISupportInitialize)(this.InputTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StateTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button AddState;
+        private System.Windows.Forms.Button RemoveState;
+        private System.Windows.Forms.TextBox AlphabetTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button MakeStepButton;
+        private System.Windows.Forms.DataGridView InputTable;
+        private System.Windows.Forms.DataGridView StateTable;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ Turing/MainForm.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Turing
+{
+    public partial class MainForm : Form
+    {
+        private static List<string> alphabet = new List<string>();
+        private static List<string> states = new List<string>();
+        private static string currentState = "q1";
+        private static int stateTableCellIndex;
+        private static int stateTableRowIndex;
+        private static bool stop = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 30; i++)
+            {
+                InputTable.Columns.Add(i.ToString(), "");
+                InputTable.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            InputTable.Rows.Add();
+            for (int i = 0; i < 30; i++)
+            {
+                InputTable.Rows[0].Cells[i].Value = "_";
+            }
+            InputTable.Enabled = false;
+            StartButton.Enabled = false;
+            MakeStepButton.Enabled = false;
+            AddState.Enabled = false;
+            RemoveState.Enabled = false;
+            StopButton.Enabled = false;
+            MessageBox.Show("Алгоритм работы программы:\n" +
+                            "Для работы нужно указать алфавит, с которым будет работать машина.\n" +
+                            "Затем ввести в ленту значения. Символ `*` является указателем на текующую ячейку в ленте. \n" +
+                            "Также нужно ввести состояния в формате символ из алфавита, направление, следущее состояние.\n" +
+                            "Пример ввода: 0>q2, 1<q1, hh - состояние для обозначения конца программы.\n", "Справка о программе",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void InputTable_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            DataGridViewColumn column = InputTable.Columns[e.ColumnIndex];
+            if (!alphabet.Contains(e.FormattedValue.ToString()))
+            {
+                MessageBox.Show("Алфавит не содержит данный символ.", "Ошибка!",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Cancel = true;
+            }      
+        }
+
+        private void StateTable_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            DataGridViewColumn column = StateTable.Columns[e.ColumnIndex];
+            if (column.Index != 0 && e.FormattedValue != "")
+            {         
+                string value = e.FormattedValue.ToString();
+                if(value.Length >= 4)
+                {
+                    //var a = value[0].ToString();
+                    if (!alphabet.Contains(value[0].ToString()))
+                    {
+                        MessageBox.Show("Алфавит не содержит данный символ.\n" +
+                                        "Пример ввода: 0>q2, 1<q1, hh - состояние для обозначения конца программы.\n", "Ошибка!",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        e.Cancel = true;
+                    }
+                    //var b = value[1].ToString();
+                    if (value[1].ToString() != "<" && value[1].ToString() != ">")
+                    {
+                        MessageBox.Show("Неверно указано направление.\n" +
+                                        "Пример ввода: 0>q2, 1<q1, hh - состояние для обозначения конца программы.\n", "Ошибка!",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        e.Cancel = true;
+                    }
+                    string substring = value.Substring(2);
+                    if (!states.Contains(substring))
+                    {
+                        MessageBox.Show("Неверно указано состояние.\n" +
+                                        "Пример ввода: 0>q2, 1<q1, hh - состояние для обозначения конца программы.\n", "Ошибка!",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        e.Cancel = true;
+                    }
+                }
+                else if (e.FormattedValue != "")
+                {
+                    e.Cancel = true;
+                }                
+            }
+        }
+
+        private void InputTable_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int fristColIndex = 0;
+            int lastColIndex = InputTable.ColumnCount - 1;
+
+            foreach (DataGridViewColumn col in InputTable.Columns)
+            {
+                col.HeaderText = "";
+            }
+            DataGridViewColumn column = InputTable.Columns[e.ColumnIndex];
+            column.HeaderText = "*";
+
+            if (e.ColumnIndex == fristColIndex)
+            {
+                InputTable.Columns[e.ColumnIndex].HeaderText = "";
+                DataGridViewColumn newColumn = new DataGridViewTextBoxColumn();
+                newColumn.Name = (int.Parse(InputTable.Columns[e.ColumnIndex].Name) - 1).ToString();
+                InputTable.Columns.Insert(0, newColumn);
+                InputTable.Columns[e.ColumnIndex].SortMode = DataGridViewColumnSortMode.NotSortable;
+                InputTable.Rows[0].Cells[0].Value = "_";
+                InputTable.Columns[e.ColumnIndex].HeaderText = "*";
+            }
+            if (e.ColumnIndex == lastColIndex)
+            {
+                InputTable.Columns[e.ColumnIndex].HeaderText = "";
+                InputTable.Columns.Add(InputTable.ColumnCount.ToString(), "");
+                InputTable.Columns[lastColIndex].SortMode = DataGridViewColumnSortMode.NotSortable;
+                InputTable.Rows[0].Cells[lastColIndex].Value = "_";
+                InputTable.Columns[lastColIndex].HeaderText = "*";
+            }
+        }
+
+        private void AlphabetTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try {
+                InputTable.Enabled = true;
+                StartButton.Enabled = true;
+                MakeStepButton.Enabled = true;
+                AddState.Enabled = true;
+                RemoveState.Enabled = true;
+                alphabet.Clear();
+                states.Clear();
+                StateTable.Columns.Clear();
+                StateTable.Columns.Add("zero", "");
+                StateTable.Columns[0].ReadOnly = true;
+                StateTable.Columns[0].SortMode = DataGridViewColumnSortMode.NotSortable;
+                StateTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                StateTable.Columns.Add("1", "q1");
+                StateTable.Columns[1].SortMode = DataGridViewColumnSortMode.NotSortable;
+                for (int i = 0; i < AlphabetTextBox.Text.Length; i++)
+                {
+                    if (alphabet.Contains(AlphabetTextBox.Text[i].ToString()))
+                    {
+                        throw new AlphabetException();
+                    }
+                    alphabet.Add(AlphabetTextBox.Text[i].ToString());
+                    StateTable.Rows.Add(alphabet[i]);
+                    StateTable.Rows[i].HeaderCell.Value = alphabet[i];
+                }
+                alphabet.Add("_");
+                StateTable.Rows.Add("_");
+                states.Add("hh");
+                states.Add("q1");
+            }           
+            catch (AlphabetException)
+            {
+                MessageBox.Show("В алфавите указаны повторяющиеся символы.", "Ошибка!",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                StartButton.Enabled = false;
+                MakeStepButton.Enabled = false;
+                AddState.Enabled = false;
+                RemoveState.Enabled = false;
+            }
+        }
+
+        public class AlphabetException : ApplicationException
+        {
+            public AlphabetException() : base() { }
+        }
+
+        private void AddState_Click(object sender, EventArgs e)
+        {
+            string nameOFNewColumn = StateTable.Columns[StateTable.Columns.Count - 1].Name;
+            StateTable.Columns.Add((int.Parse(nameOFNewColumn) + 1).ToString(), "q" + (int.Parse(nameOFNewColumn) + 1).ToString());
+            StateTable.Columns[(int.Parse(nameOFNewColumn) + 1).ToString()].SortMode = DataGridViewColumnSortMode.NotSortable;
+            states.Add("q" + (int.Parse(nameOFNewColumn) + 1).ToString());
+        }
+
+        private void RemoveState_Click(object sender, EventArgs e)
+        {
+            if (StateTable.CurrentCell.ColumnIndex > 1)
+            {
+                string nameOfColumn = StateTable.Columns[StateTable.Columns.Count - 1].Name;
+                StateTable.Columns.RemoveAt(StateTable.CurrentCell.ColumnIndex);
+                states.Remove("q" + (int.Parse(nameOfColumn) - 1).ToString());
+            }               
+        }
+
+        private void MakeStepButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StateTable.Rows[stateTableRowIndex].Cells[stateTableCellIndex].Style.BackColor = Color.White;
+                int inputTableCellIndex = 0;
+                foreach (DataGridViewColumn col in InputTable.Columns)
+                {
+                    if (col.HeaderText == "*")
+                    {
+                        col.HeaderText = "";
+                        inputTableCellIndex = col.Index;
+                        break;
+                    }
+                }
+                var cellValue = InputTable.Rows[0].Cells[inputTableCellIndex].Value;
+
+                foreach (DataGridViewColumn col in StateTable.Columns)
+                {
+                    if (col.HeaderText == currentState)
+                    {
+                        stateTableCellIndex = col.Index;
+                        break;
+                    }
+                }
+
+                string operation = "";
+                foreach (DataGridViewRow row in StateTable.Rows)
+                {
+                    if (row.Cells[0].Value.ToString() == cellValue.ToString())
+                    {
+                        if (row.Cells[stateTableCellIndex].Value != null)
+                        {
+                            operation = row.Cells[stateTableCellIndex].Value.ToString();
+                            row.Cells[stateTableCellIndex].Style.BackColor = Color.GreenYellow;
+                            stateTableRowIndex = row.Index;
+                            break;
+                        }
+                        else
+                        {
+                            row.Cells[stateTableCellIndex].Style.BackColor = Color.Red;
+                            throw new EmptyCellException();                            
+                        }
+                    }
+                }
+                var newValue = operation[0].ToString();
+                var direction = operation[1].ToString();
+                currentState = operation.Substring(2);
+
+                InputTable.Rows[0].Cells[inputTableCellIndex].Value = newValue;
+                if (direction == ">")
+                {
+                    inputTableCellIndex++;
+                }
+                else
+                {
+                    inputTableCellIndex--;
+                }
+                if (inputTableCellIndex < 0)
+                {
+                    DataGridViewColumn column = new DataGridViewTextBoxColumn();
+                    column.Name = (int.Parse(InputTable.Columns[inputTableCellIndex + 1].Name) - 1).ToString();                   
+                    InputTable.Columns.Insert(0, column);
+                    InputTable.Columns[inputTableCellIndex + 1].SortMode = DataGridViewColumnSortMode.NotSortable;
+                    InputTable.Rows[0].Cells[0].Value = "_";
+                    InputTable.Columns[inputTableCellIndex + 1].HeaderText = "*";
+                }
+                else if (inputTableCellIndex == InputTable.ColumnCount)
+                {
+                    InputTable.Columns.Add(InputTable.ColumnCount.ToString(), "");
+                    InputTable.Columns[inputTableCellIndex].SortMode = DataGridViewColumnSortMode.NotSortable;
+                    InputTable.Rows[0].Cells[inputTableCellIndex].Value = "_";
+                    InputTable.Columns[inputTableCellIndex].HeaderText = "*";
+                }
+                else
+                {
+                    InputTable.Columns[inputTableCellIndex].HeaderText = "*";
+                }                
+            }
+            catch(EmptyCellException)
+            {
+                MessageBox.Show("Данная ячейка пустая.",
+                                "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        public class EmptyCellException : ApplicationException
+        {
+            public EmptyCellException() : base() { }
+        }
+
+        private async void StartButton_ClickAsync(object sender, EventArgs e)
+        {
+            AlphabetTextBox.Enabled = false;
+            StartButton.Enabled = false;
+            MakeStepButton.Enabled = false;
+            AddState.Enabled = false;
+            RemoveState.Enabled = false;
+            StopButton.Enabled = true;
+            do
+            {
+                if(!stop)
+                {
+                    MakeStepButton_Click(sender, e);
+                    await Task.Delay(750);
+                }
+                else
+                {
+                    break;
+                }
+            } while (currentState != "hh");
+            stop = false;
+            AlphabetTextBox.Enabled = true;
+            StartButton.Enabled = true;
+            MakeStepButton.Enabled = true;
+            AddState.Enabled = true;
+            RemoveState.Enabled = true;
+            StopButton.Enabled = false;
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+}
